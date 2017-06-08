7de42b33f53f53e290adc9bf56d2bdf97cb68fbb
Leonit Kuleta <leonit_k@outlook.com>
2017-06-08 14:06:52 +0200

    Cleanup

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint
 
 mod_main = Blueprint('main', __name__)
-from werkzeug import secure_filename
-from os.path import join, dirname, realpath, os
-
-UPLOAD_FOLDER = join(dirname(realpath(__file__)), "../static/uploads/")
 
 
 @mod_main.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    ''' Renders the App index page.
+    :return:
+    '''
+    return "Welcome to the Flask App."
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/leonitk/Desktop/techstituion/venv"
+set -gx VIRTUAL_ENV "/home/leonitk/Desktop/dev/techstituion/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
